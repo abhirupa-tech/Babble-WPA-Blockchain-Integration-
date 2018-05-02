bfd7907ff37fc851583df1254eaf80ff8f6ac610
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-05-02 17:24:41 +0530

    Forms added

--- BACKEND/node_1.js
+++ BACKEND/node_1.js
@@ -2,3 +2,20 @@
 //npm install express --save adds the expressjs dependencies
 
 console.log("Hello World");
+const express = require("express");
+var app = require("express")();
+
+app.get('/',(req,res) =>{
+    res.send("App hosted on server");
+});
+
+app.listen(3000);
+
+// var http =require ("http");
+// http.createServer(function (request, response) {
+//
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Hello World\n');
+// }).listen(3000);
+
+console.log("Successfully Hosted");
