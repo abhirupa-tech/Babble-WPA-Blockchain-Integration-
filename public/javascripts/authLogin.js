92b9c269c604067c204fcf41dceb9263d040561a
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-06-13 23:41:53 +0530

    Firebase Auth

---
+++ public/javascripts/authLogin.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAQfEdbMWQy86S-GzbpaQ6GpvETRGr2Qro",
+    authDomain: "babble-login.firebaseapp.com",
+    databaseURL: "https://babble-login.firebaseio.com",
+    projectId: "babble-login",
+    storageBucket: "",
+    messagingSenderId: "386415256208"
+};
+firebase.initializeApp(config);
