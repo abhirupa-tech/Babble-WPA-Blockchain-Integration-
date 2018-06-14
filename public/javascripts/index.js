fca03c6da4edea6147fd29fa49881a9c1b1f8ed2
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-06-14 15:14:52 +0530

    Firebase signup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,3 +19,18 @@ function login(){
     });
     return false;
 }
+
+function signUp(){
+    name=$("#nameS").val();
+    mailS=$("#mailS").val();
+    pwdS=$("#pwdS").val();
+    pwdS2=$("#pwdS2").val();
+    firebase.auth().createUserWithEmailAndPassword(mailS, pwdS2).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error",errorCode,":",errorMessage);
+        // ...
+    });
+    return false;
+}
