0f743ffdd2be552ac5a8729b530abf0d8d66573d
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-07-03 21:54:05 +0530

    Reading hardcoded messages from database

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,20 @@
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-Babble.prototype.initFirebase = function() {
-    // Shortcuts to Firebase SDK features.
-    this.auth = firebase.auth();
-    this.database = firebase.database();
-    this.storage = firebase.storage();
-    this.messaging = firebase.messaging();
+function loadMessages(){
+    console.log("loadMessages running");
+    var userDataRef = firebase.database().ref("messages").orderByKey();
+    userDataRef.once("value").then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();
 
-    // Initiates Firebase auth and listen to auth state changes.
-    this.auth.onAuthStateChanged(this.authStateObserver.bind(this));
-};
+            var messageUser = childSnapshot.val().msg;
+            var user = childSnapshot.val().name;
 
-// Loads chat messages history and listens for upcoming ones.
-Babble.prototype.loadMessages = function() {
-    // Loads the last 12 messages and listen for new ones.
-    var setMessage = function(snap) {
-        var data = snap.val();
-        this.displayMessage(snap.key, data.name, data.text, data.profilePicUrl, data.imageUrl);
-    }.bind(this);
+            $("#name").append(user);
+            $("#text").append(messageUser);
 
-    this.database.ref('/messages/').limitToLast(12).on('child_added', setMessage);
-    this.database.ref('/messages/').limitToLast(12).on('child_changed', setMessage);
-};
+        });
+    });
+
+}
+
+loadMessages();
